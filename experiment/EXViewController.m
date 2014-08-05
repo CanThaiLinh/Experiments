c29ce8f8ff625fb3ec0c0574551bbb72868687a7
Stefan Kendall <stefankendall@gmail.com>
2014-08-05 10:44:28 -0400

    IT WORKS.

--- experiment/EXViewController.m
+++ experiment/EXViewController.m
@@ -11,22 +11,22 @@
     self.scrollView.showsVerticalScrollIndicator = NO;
     self.scrollView.pagingEnabled = YES;
     self.scrollView.clipsToBounds = NO;
-    self.scrollView.frame = CGRectMake(0, 0, 320, self.scrollView.frame.size.height);
 
     float height = self.scrollView.frame.size.height;
 
-    int spacerWidth = 30;
-    float width = self.scrollView.frame.size.width -  2 * spacerWidth;
-
-    UIView *red = [[UIView alloc] initWithFrame:CGRectMake(spacerWidth, 0, width, height)];
+    UIView *red = [[UIView alloc] initWithFrame:CGRectMake(10, 0, 260, height)];
     [red setBackgroundColor:[UIColor redColor]];
 
-    UIView *green = [[UIView alloc] initWithFrame:CGRectMake(320 + spacerWidth, 0, width, height)];
+    UIView *green = [[UIView alloc] initWithFrame:CGRectMake(290, 0, 260, height)];
     [green setBackgroundColor:[UIColor greenColor]];
+
+    UIView *blue= [[UIView alloc] initWithFrame:CGRectMake(570, 0, 260, height)];
+    [blue setBackgroundColor:[UIColor blueColor]];
 
     [self.scrollView addSubview:red];
     [self.scrollView addSubview:green];
-    self.scrollView.contentSize = CGSizeMake(320 * 2, self.scrollView.frame.size.height);
+    [self.scrollView addSubview:blue];
+    self.scrollView.contentSize = CGSizeMake(320 * 3, self.scrollView.frame.size.height);
 }
 
 @end
