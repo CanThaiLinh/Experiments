bb38e7d9ef593819effc535d1502fcdb7e75328f
Stefan Kendall <stefankendall@gmail.com>
2014-08-06 09:34:00 -0400

    Swipe not working on cards after reveal

--- experiment/EXViewController.h
+++ experiment/EXViewController.h
@@ -2,11 +2,13 @@
 
 @class ClipView;
 
-@interface EXViewController : UIViewController {
+@interface EXViewController : UIViewController <UIGestureRecognizerDelegate> {
 }
 
 @property(weak, nonatomic) IBOutlet ClipView *clipView;
 @property(weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @property(nonatomic, strong) NSMutableArray *cardViews;
+@property(nonatomic) BOOL swiping;
+@property(nonatomic) BOOL cardRevealed;
 @end
--- experiment/EXViewController.m
+++ experiment/EXViewController.m
@@ -6,25 +6,63 @@
 
 - (void)viewDidLoad {
     self.clipView.scrollView = self.scrollView;
+    self.cardRevealed = NO;
 
     self.scrollView.showsVerticalScrollIndicator = NO;
     self.scrollView.showsHorizontalScrollIndicator = NO;
     self.scrollView.pagingEnabled = YES;
     self.scrollView.clipsToBounds = NO;
+    self.scrollView.canCancelContentTouches = NO;
+
+    UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(cardPanned:)];
+    [recognizer setDelegate:self];
+    [self.scrollView addGestureRecognizer:recognizer];
 
     [self buildCards];
     [self adjustCardSizes];
 }
 
+- (void)cardPanned:(UIPanGestureRecognizer *)gesture {
+    if (gesture.state == UIGestureRecognizerStateBegan) {self.swiping = NO;}
+    CGPoint v = [gesture velocityInView:self.scrollView];
+    if (abs(v.y) >= 50 && !self.swiping && !self.cardRevealed) {
+        NSLog(@"Revealing");
+        self.swiping = YES;
+        [gesture cancelsTouchesInView];
+        [self revealCard];
+    }
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
+}
+
+- (void)revealCard {
+    self.cardRevealed = YES;
+    int page = (int) (self.scrollView.contentOffset.x / self.scrollView.frame.size.width);
+    UIView *card = self.cardViews[page];
+    card.center = CGPointMake(card.center.x, card.center.y - 100);
+}
+
 - (void)buildCards {
     const int CARD_COUNT = 5;
     self.cardViews = [@[] mutableCopy];
+    UISwipeGestureRecognizer *swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swiped:)];
+    UITapGestureRecognizer *testTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(swiped:)];
+    [swipeGestureRecognizer setDirection:UISwipeGestureRecognizerDirectionDown];
+    [swipeGestureRecognizer setDelegate:self];
     for (int i = 0; i < CARD_COUNT; i++) {
         UIView *view = [UIView new];
+        [view addGestureRecognizer:swipeGestureRecognizer];
+        [view addGestureRecognizer:testTap];
         [view setBackgroundColor:[Colors randomColor]];
         [self.cardViews addObject:view];
         [self.scrollView addSubview:view];
     }
+}
+
+- (void)swiped:(UISwipeGestureRecognizer *)gesture {
+    NSLog(@"SWIPE");
 }
 
 - (void)adjustCardSizes {
