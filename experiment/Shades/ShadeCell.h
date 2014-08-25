bf53633aa0cf7712a06534061dd4b004b5ea4f34
Stefan Kendall <stefankendall@gmail.com>
2014-08-25 14:34:03 -0400

    Wired view

--- experiment/Shades/ShadeCell.h
+++ experiment/Shades/ShadeCell.h
@@ -1,2 +1,13 @@
-@interface ShadeCell : UITableViewCell
+@class SpotData;
+
+@interface ShadeCell : UITableViewCell {}
+@property (weak, nonatomic) IBOutlet UILabel *longNamle;
+@property (weak, nonatomic) IBOutlet UILabel *shortName;
+@property (weak, nonatomic) IBOutlet UILabel *fullAssociatedName;
+
+@property (weak, nonatomic) IBOutlet UILabel *change;
+@property (weak, nonatomic) IBOutlet UILabel *price;
+
+@property(nonatomic, strong) SpotData *spotData;
+
 @end
--- experiment/Shades/ShadeCell.m
+++ experiment/Shades/ShadeCell.m
@@ -1,5 +1,10 @@
 #import "ShadeCell.h"
+#import "SpotData.h"
 
 @implementation ShadeCell
+
+- (void)setSpotData:(SpotData *)spotData {
+    _spotData = spotData;
+}
 
 @end
